3e2258fa9ff63b5858d97a59654e04b6451c221d
unknown <kirschjanosmark@gmail.com>
2018-12-28 21:40:25 +0100

    Játék elindulása, térkép, a terv, hogy a falak panel/panelek segítségével lesznek ábrázolva

--- 3dLabirintus/3dLabirintus/Form1.cs
+++ 3dLabirintus/3dLabirintus/Form1.cs
@@ -14,6 +14,12 @@ namespace _3dLabirintus
     {
 
         static Panel hatter;
+        static Label terkep;
+
+        static int i = 0, j = 0;
+
+        static char[,] palya;
+        static int palyaSzelesseg = 8, palyaMagassag = 8;
 
         public Form1()
         {
@@ -24,22 +30,107 @@ namespace _3dLabirintus
         {
             Text = "3D Labirintus";
 
+            palya = new char[palyaMagassag, palyaSzelesseg];
+            palyaFeltoltese(palyaMagassag, palyaSzelesseg);
+
+            hatter = new Panel();
+            terkep = new Label();
+
             jatekInditas();
             Controls.Add(hatter);
+            hatter.Controls.Add(terkep);
+
+            KeyDown += new KeyEventHandler(billentyuLenyomas);
+
+        }
+
+        private void billentyuLenyomas(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.M)
+            {
+                if (terkep.Visible)
+                {
+                    terkep.Visible = false;
+                }
+                else
+                {
+                    terkep.Visible = true;
+                }
+            }
+        }
+
+        private void palyaFeltoltese(int palyaMagassag, int palyaSzelesseg)
+        {
+            palyaUresFeltoltese(palyaMagassag, palyaSzelesseg);
+            palyaRajzolasa(palyaMagassag, palyaSzelesseg);
+        }
+
+        private static string palyaRajzolasa(int palyaMagassag, int palyaSzelesseg)
+        {
+            string szoveg = "";
+
+            for (i = 0; i < palyaMagassag; i++)
+            {
+                for (j = 0; j < palyaSzelesseg; j++)
+                {
+                    szoveg += palya[i, j];
+                }
+                szoveg += '\n';
+            }
+
+            return szoveg;
+
+        }
+
+        private static string palyaRajzolasa()
+        {
+            string szoveg = "";
+
+            for (i = 0; i < palyaMagassag; i++)
+            {
+                for (j = 0; j < palyaSzelesseg; j++)
+                {
+                    szoveg += palya[i, j];
+                }
+                szoveg += '\n';
+            }
+
+            return szoveg;
+
+        }
+
+        private void palyaUresFeltoltese(int palyaMagassag, int palyaSzelesseg)
+        {
+            for (i = 0; i < palyaMagassag; i++)
+            {
+                for (j = 0; j < palyaSzelesseg; j++)
+                {
+                    palya[i, j] = '-';
+                }
+            }
 
         }
 
         private static void jatekInditas()
         {
-            hatter = new Panel();
             hatter.BackColor = Color.FromArgb(100, 135, 206, 255);
             hatter.Dock = DockStyle.Fill;
 
-            /*Pen ceruza = new Pen(Color.Black, 20);
-            Graphics grafika = hatter.CreateGraphics();
+            terkep.Width = 100;
+            terkep.Height = 100;
+            terkep.Text = palyaRajzolasa();
 
-            grafika.DrawLine(ceruza, 25, 52, 32, 42);*/
+           // hatter.Paint += new PaintEventHandler(vonalRajzolasa);
         }
+
+        /*private static void vonalRajzolasa(object sender, PaintEventArgs e)
+        {
+            Pen blackpen = new Pen(Color.Black, 1);
+
+            Graphics g = e.Graphics;
+
+            g.DrawLine(blackpen, 0, 0, 200, 300);
+        }*/
 
         public static double tavolsagSzamitasa(Pont elsoPont, Pont masodikPont)
         {
